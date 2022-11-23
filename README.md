eb3ca88a15931a48f340c7d7b64dd4012ccbeac7
krajco <xkrajc17@stud.fit.vutbr.cz>
2022-11-23 22:21:34 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# BMS
-bms
+# bms
---
+++ coder.h
@@ -0,0 +1,17 @@
+#ifndef BMS_CODER_H
+#define BMS_CODER_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <unistd.h>
+#include <vector>
+#include<iterator>
+using namespace std;
+
+
+vector<int> getRSC(vector<int> rsc, vector<int> data, unsigned int i);
+void encode(vector<int> data, vector<int> interLeaver);
+
+#endif //BMS_CODER_H
